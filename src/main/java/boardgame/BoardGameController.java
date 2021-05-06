71d892bd6b93274039b938fcf8fff5f995908e41
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-05-07 00:40:16 +0200

    Added BoardGameController class containing the initializing methods

---
+++ src/main/java/boardgame/BoardGameController.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+
+public class BoardGameController {
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private void initialize() {
+        for (int i = 0; i < board.getRowCount(); i++) {
+            for (int j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare();
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare() {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        var piece = new Ellipse(32, 47);
+        piece.setFill(Color.DARKGRAY);
+        square.getChildren().add(piece);
+        return square;
+    }
+
+}
