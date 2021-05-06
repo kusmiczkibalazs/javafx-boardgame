d02382fd551cfd9d18696b3b6c26a0d55b1e5044
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-05-07 00:37:28 +0200

    Added BoardGameApplication class containing the window settings

---
+++ src/main/java/boardgame/BoardGameApplication.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BoardGameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/board.fxml"));
+        stage.setTitle("Kavics kapkodó");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
