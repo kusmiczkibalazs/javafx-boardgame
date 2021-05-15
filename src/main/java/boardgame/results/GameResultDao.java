e86fab948225e57847fb9256e23cb1edbb0d714d
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-05-15 19:01:35 +0200

    Added GameResultDao class containing the database operations

---
+++ src/main/java/boardgame/results/GameResultDao.java
@@ -0,0 +1,35 @@
+package boardgame.results;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+@RegisterBeanMapper(GameResult.class)
+public interface GameResultDao {
+
+    @SqlUpdate("""
+        CREATE TABLE IF NOT EXISTS results (
+            id IDENTITY PRIMARY KEY,
+            firstPlayer VARCHAR NOT NULL,
+            secondPlayer VARCHAR NOT NULL,
+            winner VARCHAR NOT NULL,
+            gameDate TIMESTAMP NOT NULL
+        )
+        """
+    )
+    void createTable();
+
+    @SqlUpdate("""
+       INSERT INTO results (firstPlayer, secondPlayer, winner, gameDate)
+       VALUES (:firstPlayer, :secondPlayer, :winner, :gameDate)
+       """)
+    @GetGeneratedKeys
+    long insertResult(@BindBean GameResult gameResult);
+
+    @SqlQuery("SELECT * FROM results ORDER BY gameDate")
+    List<GameResult> listGameResults();
+}
