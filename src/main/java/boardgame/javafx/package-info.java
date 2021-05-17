efdb8ae3ec308ac5633876a84a2efc21c6045dff
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-05-17 22:19:54 +0200

    Changed inline tag to '@code'

--- src/main/java/boardgame/javafx/package-info.java
+++ src/main/java/boardgame/javafx/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains the {@link boardgame.javafx.Main} class and the package of the controller classes.
+ * Contains the {@code Main} class and the package of the controller classes.
  */
 package boardgame.javafx;
